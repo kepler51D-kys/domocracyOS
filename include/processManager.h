73a5de4e34ec02560d9d768c93734450777e6e03
Kepler51D-kys <Anonymous5939@protonmail.com>
2025-10-13 11:34:05 +0100

    started process manager

---
+++ include/processManager.h
@@ -0,0 +1,8 @@
+#ifndef PROCESSMANAGER_H
+#define PROCESSMANAGER_H
+typedef struct {
+    void* pc;
+    // todo: registers
+} process;
+
+#endif
---
+++ libs/processManager.c
@@ -0,0 +1,5 @@
+#include "processManager.h"
+#include <stdint.h>
+
+uint32_t processInc = 0;
+uint32_t quantumLength = 10;
