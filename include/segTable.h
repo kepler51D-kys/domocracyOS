64a7b15b1cc2135e5c336e9cc2e3580b13486d1a
Kepler51D-kys <Anonymous5939@protonmail.com>
2025-10-13 10:50:57 +0100

    changed git ignore

--- include/segTable.h
+++ include/segTable.h
@@ -1,4 +1,10 @@
 #ifndef SEGTABLE_H
 #define SEGTABLE_H
+#include <stdint.h>
+
+typedef struct {
+    void* start;
+    uint32_t length;
+} segment;
 
 #endif
--- libs/segTable.c
+++ libs/segTable.c
@@ -1,1 +1,5 @@
 #include "segTable.h"
+
+segment segment_init(void* start, uint32_t length) {
+    return (segment){start,length};
+}
