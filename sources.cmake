ff6911f863a6990c868491aa417867ba3c495d9c
PilkeySEK <PilkeySEK@gmx.de>
2025-10-14 15:27:32 +0200

    improve things

---
+++ sources.cmake
@@ -0,0 +1,11 @@
+set(C_SOURCES
+    src/kernel.c
+    libs/segTable.c
+    libs/print.c
+    libs/processManager.c
+)
+
+set(NASM_SOURCES
+    src/boot.asm
+    src/v86.asm
+)
--- src/kernel.c
+++ src/kernel.c
@@ -1,6 +1,9 @@
 #include "print.h"
 
+extern int detect_v86();
+
 int main() {
     printf("hello world: %s : goodbye","hello");
+    printf("   v86?: %s", detect_v86() ? "true" : "false");
     while(1);
 }
