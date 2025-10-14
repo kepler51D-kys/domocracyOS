845baa134e988b4b8b8af1feef473d6f2e9bc0ba
PilkeySEK <PilkeySEK@gmx.de>
2025-10-14 15:54:32 +0200

    t

--- src/kernel.c
+++ src/kernel.c
@@ -3,13 +3,6 @@
 extern int detect_v86();
 
 int main() {
-    volatile int x = detect_v86();
-    volatile int n;
-    if(x == 1) {
-        n = 0x555555;
-    }
-    printf("%d", n);
-    // printf("hello world: %s : goodbye","hello");
-    // printf("   v86?: %d", detect_v86());
+    printf("   v86?: %d", detect_v86());
     while(1);
 }
