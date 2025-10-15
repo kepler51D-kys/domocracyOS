548e3307159078033a473f7c6cfda807dc489cb0
Kepler51D-kys <Anonymous5939@protonmail.com>
2025-10-15 15:52:50 +0100

    nothing much

--- libs/graphics.c
+++ libs/graphics.c
@@ -13,6 +13,7 @@ void graphics_init() {
         return;
     }
     framebuffer = (uint8_t*)(BgaGetFramebufferAddress(device));
+    
     for (int i = 0; i < GRAPHICS_WIDTH*GRAPHICS_HEIGHT*3; i++) {
         buffers[0][i] = 0;
         buffers[1][i] = 0;
