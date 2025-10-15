470250d4cf712636d4a79b88d7e9d4869469c2ae
Kepler51D-kys <Anonymous5939@protonmail.com>
2025-10-15 08:21:55 +0100

    vesa graphics

--- src/kernel.c
+++ src/kernel.c
@@ -1,12 +1,23 @@
 #include "print.h"
-#include "segTable.h"
+#include "bga.h"
+#include "graphics.h"
 
 int main() {
-    // mem_init();
-    float temp = 4.6;
-    temp *= 2;
-    int x = (int)temp;
-    // printf("pointer location : %p\n",malloc(50));
-    printf("float test : %d\n",x);
+    printf("Initializing VESA/VBE...");
+    if(BgaIsAvailable()) {
+        printf("BGA is available!");
+    }
+    else {
+        printf("BGA was not detected but lets see if it works.");
+    }
+
+    volatile uint8_t* framebuffer = graphics_init();
+
+    for(int y = 0; y < GRAPHICS_HEIGHT * 3; y++) {
+        for(int x = 0; x < GRAPHICS_WIDTH; x++) {
+            int c = x == 0 ? 0x00 : x % 2 == 0 ? 0xFF : 0x00;
+            putpixel(x, y, x == 0 ? 0xFF : c, c, c, framebuffer);
+        }
+    }
     while(1);
 }
